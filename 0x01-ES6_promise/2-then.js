88603695cc5a085d097c1517429da2faa9509f80
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-11-28 21:33:20 +0300

    Appended three handlers to the function  Promise resolves Promise rejects Finally

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,11 +3,10 @@ export default function handleResponseFromAPI(promise) {
     promise
       .then(() => {
         resolve({ status: 200, body: 'success' });
+        console.log('Got a response from the API');
       })
       .catch(() => {
         reject(Error());
-      })
-      .finally(() => {
         console.log('Got a response from the API');
       });
   });
