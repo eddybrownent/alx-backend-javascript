8b7210c4cecf1308351673c3e00f09655b102d9a
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-11-29 13:39:14 +0300

    worked on the corrected output

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({ status: 200, body: 'success' }))
-    .catch(() => Promise.reject(Error({})))
-    .finally(() => {
-      console.log('Got a response from the API');
-    });
+    .catch(() => Promise.reject(Error()))
+    .finally(() =>
+      console.log('Got a response from the API'));
 }
