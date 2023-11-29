607fd11ffba6775e67bc37983bde0571114c9949
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-11-29 10:58:40 +0300

    Handling  multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,17 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  let result = [];
+
+  try {
+    const userProfile = await signUpUser(firstName, lastName);
+    result = [...result, { status: 'fulfilled', value: userProfile }];
+    await uploadPhoto(fileName);
+    result = [...result, { status: 'fulfilled', value: 'Photo uploaded successfully' }];
+  } catch (error) {
+    result = [...result, { status: 'rejected', value: 'Error Occured' }];
+  }
+
+  return result;
+}
