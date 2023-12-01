47f0d575cf8f9ace9bc2a9c62e0e6bda7aa6afc3
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-12-01 10:23:44 +0300

    Displaying the currency name and code and a simple conversion calculation

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,36 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (value instanceof Currency) {
+      this._currency = value;
+    } else {
+      throw new Error();
+    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
