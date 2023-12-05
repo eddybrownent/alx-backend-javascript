51b198590775ad6fcfc9b621298daab2975c89d8
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-12-05 15:04:19 +0300

    Fixing the to the required correct output

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
 export default function cleanSet(set, startString) {
-  if (!startString || !startString.length) {
-    return '';
-  }
-
   let resultString = '';
+
+  if (!startString || !startString.length) {
+    return resultString;
+  }
 
   for (const value of set) {
     if (value.startsWith(startString)) {
@@ -11,5 +11,5 @@ export default function cleanSet(set, startString) {
     }
   }
 
-  return resultString.slice(0, resultString.length - 1);
+  return resultString.slice(0, -1);
 }
