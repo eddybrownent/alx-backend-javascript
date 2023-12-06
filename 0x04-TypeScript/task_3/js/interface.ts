53c17e255f62d5dcdb82f1de8cd89beb08577a7a
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-12-07 00:11:45 +0300

    Ambient Namespaces | containing the type declarations for each crud function

---
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -0,0 +1,9 @@
+// Define RowID as a type
+export type RowID = number;
+
+// Define RowElement as an interface
+export interface RowElement {
+  firstName: string;
+  lastName: string;
+  age?: number;
+}
