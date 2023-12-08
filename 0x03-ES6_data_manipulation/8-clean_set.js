250a33640dd226706ce41bd0a894bd0305c624aa
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-12-08 21:41:15 +0300

    Function that retuns a srting of all the set values

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,10 @@
 export default function cleanSet(set, startString) {
   let resultString = '';
 
+  if (!startString || !startString.length) {
+    return '"Execution not successful"';
+  }
+
   for (const value of set) {
     if (value.startsWith(startString)) {
       resultString += `${value.slice(startString.length)}-`;
