44bdfbaad2fb0404e7bafdfa974122c0b6ca58b2
edwin karanja mburu <ekaranja.28@gmail.com>
2024-01-30 00:30:31 +0300

    Using Process stdin- fixed the program to output the reqiured format

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,12 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data.toString()}`);
-
-  process.exit(process.stdout.write('This important software is now closing\n'));
+  process.stdout.write(`Your name is: ${data.toString()}\n`);
+  process.exit();
 });
+
+if (!process.stdin.isTTY) {
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
