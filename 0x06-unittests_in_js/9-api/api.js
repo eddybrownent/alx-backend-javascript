e54dd9c40e0b4653ae44063c8eb56a23252000f5
edwin karanja mburu <ekaranja.28@gmail.com>
2024-02-03 09:33:19 +0300

    added an endpoint that takes in a cart id and validates if its only a number and added test cases using the Mocha testing framework

---
+++ 0x06-unittests_in_js/9-api/api.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+const port = 7865;
+
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
+});
+
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome to the payment system');
+});
+
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  res.status(200).send(`Payment methods for cart ${req.params.id}`);
+});
+
+module.exports = app;
---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,45 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('API Tests', () => {
+  describe('Index Page', () => {
+    it('should return correct status code', (done) => {
+      request('http://localhost:7865', (error, response, body) => {
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+    });
+
+    it('should return correct result', (done) => {
+      request('http://localhost:7865', (error, response, body) => {
+        expect(body).to.equal('Welcome to the payment system');
+        done();
+      });
+    });
+
+    it('should have content type set to text/html', (done) => {
+      request('http://localhost:7865', (error, response, body) => {
+        expect(response.headers['content-type']).to.include('text/html');
+        done();
+      });
+    });
+  });
+
+  describe('Cart Page', () => {
+    it('should return correct status code when id is a number', (done) => {
+      const cartId = 142;
+      request(`http://localhost:7865/cart/${cartId}`, (error, response, body) => {
+        expect(response.statusCode).to.equal(200);
+	done();
+      });
+    });
+
+    it('should return 404 status code when id is Not a number', (done) => {
+      const notNumber = 'string'
+      request(`http://localhost:7865/cart/${notNumber}`, (error, response, body) => {
+        expect(response.statusCode).to.equal(404);
+	done();
+      });
+    });
+  });
+});
